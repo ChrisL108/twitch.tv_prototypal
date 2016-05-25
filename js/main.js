e6598ec06c93e6849dafe4ab25e3999a4cca8e5d
ChrisL108 <chrislacaille5@gmail.com>
2016-05-25 00:00:11 -0500

    add functionality for future 'filter' buttons

--- js/main.js
+++ js/main.js
@@ -1,34 +1,24 @@
 
-
-
 //--- Constructor for streamers
 function Streamer(streamerName) {
 	this.streamerName = streamerName;
+	this.getUser();
 };
 
-//--- CREATE_USER
-Streamer.prototype.createUser = function(name, info, logo, streaming) {
-
-	let newListItem = "<li class='"+ streaming + "'>" +
-					  "<img src ='"+ logo +"' class='user-logos'> " +
-					  "<h4><a href='https://www.twitch.tv/" + name + "'>" +
-					  name + "</a></h4>" +
-					  "<span class='stream-info'>" + info +
-					  "</span></li>";
-	$("#streamers").prepend(newListItem);
-};
+//--- MAKE_URL
+Streamer.prototype.makeURL = function(type, name) {
+  return 'https://api.twitch.tv/kraken/' +
+  		 type + '/' + name + '?callback=?';
+}
 
 //--- GET_USER
 Streamer.prototype.getUser = function() {
 
 	// getJSON for USER data
-	$.getJSON(Streamer.prototype
-					  .makeURL('users', this.streamerName), function(data) {
+	$.getJSON(Streamer.prototype.makeURL('users', this.streamerName), function(data) {
 
 		// getJSON for user STREAM data
-		var getStreamData = $.getJSON(
-							  Streamer.prototype
-							 		  .makeURL('streams', data.name), function(streamData) {
+		var getStreamData = $.getJSON(Streamer.prototype.makeURL('streams', data.name), function(streamData) {
 
 			// If user has no logo use avatar
 			let userLogo = data.logo || "https://www.dropbox.com/s/fatgktf4x2lqk23/avatar.png?raw=1" ;
@@ -53,29 +43,47 @@ Streamer.prototype.getUser = function() {
 
 };
 
-//--- MAKE_URL
-Streamer.prototype.makeURL = function(type, name) {
-  return 'https://api.twitch.tv/kraken/' +
-  		 type + '/' + name + '?callback=?';
-}
+//--- CREATE_USER
+Streamer.prototype.createUser = function(name, info, logo, streaming) {
 
-// List of streamers to add
+	let newListItem = "<li class='"+ streaming + "'>" +
+					  "<img src ='"+ logo +"' class='user-logos img-fluid'> " +
+					  "<h4><a href='https://www.twitch.tv/" + name + "'>" +
+					  name + "</a></h4>" +
+					  "<span class='stream-info'>" + info +
+					  "</span></li>";
+	$("#streamers").prepend(newListItem);
+};
+
+// List of streamers to add - ADD STREAMERS HERE
  let streamers = ["freecodecamp", "storbeck", "terakilobyte",
  "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff",
  "ESL_SC2", "OgamingSC2", "cretetion"];
 
 // Create new array with streamer 'objects'
- var streamerMap = streamers.map(function(elem, index) {
+ var streamerMap = streamers.map(function(elem) {
  	var userToAdd = new Streamer(elem);
  	return userToAdd;
  });
 
-// Call getUser() for each streamer object
-streamerMap.forEach(function(elem) {
-	elem.getUser();
+
+
+
+// Filter Online / Offline - ( Event Handler )
+
+$("#filter").on("click", "button", function() {
+  // ALL
+  if ($(this).html() === "All") {
+    $(".offline, .online").show();
+  }
+  //ONLINE
+  else if ($(this).html() === "Online") {
+    $(".offline").hide();
+    $(".online").show();
+  } 
+  //OFFLINE
+  else {  
+    $(".online").hide();
+    $(".offline").show();
+  }
 });
-
-
-
-
-
